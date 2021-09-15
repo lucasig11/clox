286f681cb1f4b9b1e2851a61c03a5dcc84b7174a
lucas <lucasfonsecard@icloud.com>
2021-09-15 19:35:32 -0300

    fix: pass instruction offset by value

--- src/debug.c
+++ src/debug.c
@@ -6,32 +6,32 @@ void disassemble_chunk(Chunk *chunk, const char *name) {
     printf("== %s ==\n", name);
 
     for (int offset = 0; offset < chunk->length;)
-        disassemble_instruction(chunk, &offset);
+        offset = disassemble_instruction(chunk, offset);
 }
 
-static void simple_instruction(const char *name, int *offset) {
+static int simple_instruction(const char *name, int offset) {
     printf("%s\n", name);
-    *offset += 1;
+    return offset += 1;
 }
 
-static void const_instruction(const char *name, Chunk *chunk, int *offset) {
-    uint8_t constant = chunk->code[*offset + 1];
+static int const_instruction(const char *name, Chunk *chunk, int offset) {
+    uint8_t constant = chunk->code[offset + 1];
     printf("%-16s %4d '", name, constant);
     print_value(chunk->constants.values[constant]);
     printf("'\n");
-    *offset += 2;
+    return offset += 2;
 }
 
-void disassemble_instruction(Chunk *chunk, int *offset) {
-    printf("%04d ", *offset);
+int disassemble_instruction(Chunk *chunk, int offset) {
+    printf("%04d ", offset);
 
-    if (*offset > 0 && chunk->lines[*offset] == chunk->lines[*offset - 1]) {
+    if (offset > 0 && chunk->lines[offset] == chunk->lines[offset - 1]) {
         printf("   | ");
     } else {
-        printf("%4d ", chunk->lines[*offset]);
+        printf("%4d ", chunk->lines[offset]);
     }
 
-    uint8_t instruction = chunk->code[*offset];
+    uint8_t instruction = chunk->code[offset];
     switch (instruction) {
     case OP_RETURN:
         return simple_instruction("OP_RETURN", offset);
@@ -39,6 +39,6 @@ void disassemble_instruction(Chunk *chunk, int *offset) {
         return const_instruction("OP_CONST", chunk, offset);
     default:
         printf("Unknown opcode %d\n", instruction);
-        *offset += 1;
+        return offset += 1;
     }
 }
