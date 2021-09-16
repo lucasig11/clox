1ed995e9648dbd8ce3bc3e9e54bd44fd986c9fbd
lucas <lucasfonsecard@icloud.com>
2021-09-15 22:22:11 -0300

    refactor: remove handmade bytecode chunk from main

--- src/main.c
+++ src/main.c
@@ -5,19 +5,6 @@
 
 int main(int argc, char *argv[]) {
     init_VM();
-
-    // Handmade chunk
-    Chunk chunk;
-    init_chunk(&chunk);
-    int constant = add_constant(&chunk, 1.2);
-    write_chunk(&chunk, OP_CONST, 123);
-    write_chunk(&chunk, constant, 123);
-    write_chunk(&chunk, OP_NEGATE, 123);
-    write_chunk(&chunk, OP_RETURN, 123);
-
-    interpret(&chunk);
-
     free_VM();
-    free_chunk(&chunk);
     return 0;
 }
