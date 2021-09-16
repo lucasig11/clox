fbb6dddd98fc3c63b6e7a71dc2a689e1ceb6b43b
lucas <lucasfonsecard@icloud.com>
2021-09-15 21:23:39 -0300

    feat: add stack for the VM instructions

--- include/vm.h
+++ include/vm.h
@@ -2,11 +2,17 @@
 #define CLOX_VM_H
 
 #include "chunk.h"
+#include "value.h"
+
+#define STACK_MAX 256
 
 typedef struct {
     Chunk *chunk;
     // Instruction pointer
     uint8_t *ip;
+    Value stack[STACK_MAX];
+    // Address of the first empty slot in the stack
+    Value *stack_top;
 } VM;
 
 typedef enum {
@@ -18,5 +24,9 @@ typedef enum {
 void init_VM();
 void free_VM();
 InterpretResult interpret(Chunk *);
+
+// stack functions
+void push(Value value);
+Value pop();
 
 #endif
--- src/vm.c
+++ src/vm.c
@@ -5,7 +5,9 @@
 
 VM vm;
 
-void init_VM() {}
+static void reset_stack() { vm.stack_top = vm.stack; }
+
+void init_VM() { reset_stack(); }
 
 void free_VM() {}
 
@@ -14,18 +16,27 @@ static InterpretResult run() {
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
     while (true) {
 #ifdef DEBUG_TRACE_EXECUTION
+        printf("        ");
+        for (Value *slot = vm.stack; slot < vm.stack_top; slot++) {
+            printf("[");
+            print_value(*slot);
+            printf("]");
+        }
+        printf("\n");
         disassemble_instruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
 #endif
         uint8_t instruction;
         switch (instruction = READ_BYTE()) {
         case OP_CONST: {
             Value constant = READ_CONSTANT();
-            print_value(constant);
-            printf("\n");
+            push(constant);
             break;
         }
-        case OP_RETURN:
+        case OP_RETURN: {
+            print_value(pop());
+            printf("\n");
             return INTERPRET_OK;
+        }
         }
     }
 #undef READ_BYTE
@@ -36,4 +47,14 @@ InterpretResult interpret(Chunk *chunk) {
     vm.chunk = chunk;
     vm.ip = vm.chunk->code;
     return run();
+}
+
+void push(Value value) {
+    *vm.stack_top = value;
+    vm.stack_top++;
+}
+
+Value pop() {
+    vm.stack_top--;
+    return *vm.stack_top;
 }
