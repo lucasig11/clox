54366209d850cfacb35901a4fdd44e1e03149ee7
lucas <lucasfonsecard@icloud.com>
2021-09-18 16:56:27 -0300

    feat: implement parser and report syntax errors

--- includes/compiler.h
+++ includes/compiler.h
@@ -1,6 +1,8 @@
 #ifndef CLOX_COMPILER_H
 #define CLOX_COMPILER_H
 
-void compile(const char *src);
+#include "chunk.h"
+
+bool compile(const char *src, Chunk *chunk);
 
 #endif
--- src/vm.c
+++ src/vm.c
@@ -68,7 +68,21 @@ InterpretResult run() {
 }
 
 InterpretResult interpret(const char *source) {
-    compile(source);
+    Chunk chunk;
+    init_chunk(&chunk);
+
+    if (!compile(source, &chunk)) {
+        free_chunk(&chunk);
+        return INTERPRET_COMPILE_ERROR;
+    }
+
+    vm.chunk = &chunk;
+    vm.ip = vm.chunk->code;
+
+    InterpretResult result = run();
+
+    free_chunk(&chunk);
+
     return INTERPRET_OK;
 }
 
