2a00ba58dbd7d2072e574018e83948b19e433ca5
lucas <lucasfonsecard@icloud.com>
2021-09-19 22:34:58 -0300

    feat: add macros to cast between C and Lox types

--- includes/value.h
+++ includes/value.h
@@ -3,7 +3,33 @@
 
 #include "common.h"
 
-typedef double Value;
+typedef enum {
+  VAL_BOOL,
+  VAL_NIL,
+  VAL_NUMBER,
+} ValueType;
+
+typedef struct {
+  ValueType type;
+  union {
+    bool boolean;
+    double number;
+  } as;
+} Value;
+
+// Macros to cast LoxValues to C types
+#define AS_BOOL(value) ((value).as.boolean)
+#define AS_NUMBER(value) ((value).as.number)
+
+// Macros to verify LoxValue type
+#define IS_BOOL(value) ((value).type == VAL_BOOL)
+#define IS_NIL(value) ((value).type == VAL_NIL)
+#define IS_NUMBER(value) ((value).type == VAL_NUMBER)
+
+// Macros to create LoxValues from C types
+#define BOOL_VAL(value) 	((Value) {VAL_BOOL, {.boolean = value}})
+#define NUMBER_VAL(value) 	((Value) {VAL_NUMBER, {.number = value}})
+#define NIL_VAL 			((Value) {VAL_NIL, {.number = 0}})
 
 typedef struct {
   int cap;
