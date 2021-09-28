b336f226b09ecfa08a8bf484fab598dd78481e2c
lucas <lucasfonsecard@icloud.com>
2021-09-27 23:06:59 -0300

    feat: add hash-table

---
+++ includes/table.h
@@ -0,0 +1,25 @@
+#ifndef CLOX_TABLE_H
+#define CLOX_TABLE_H
+
+#include "common.h"
+#include "value.h"
+
+typedef struct {
+  // The key is always a string, so we can store the pointer directly
+  // to the string object in the heap.
+  ObjString *key;
+  Value value;
+} Entry;
+
+typedef struct {
+  // The ratio between `count` and `capacity` is the table's load factor.
+  int count;
+  int capacity;
+  // Head of the entries list.
+  Entry *entries;
+} Table;
+
+void init_table(Table *table);
+void free_table(Table *table);
+
+#endif
