3e2edb03a1a8658124059ae7f4bb6e11a7449aec
lucas <lucasfonsecard@icloud.com>
2021-09-29 14:06:20 -0300

    feat: add block statement

--- src/compiler.c
+++ src/compiler.c
@@ -159,6 +159,10 @@ static void end_compiler() {
 #endif
 }
 
+static void begin_scope() { current->scope_depth++; }
+
+static void end_scope() { current->scope_depth--; }
+
 /* Signatures */
 static void expression();
 static void statement();
@@ -353,6 +357,13 @@ static ParseRule *get_rule(TokenType type) { return &rules[type]; }
 
 static void expression() { parse_precedence(PREC_ASSIGNMENT); }
 
+static void block() {
+  while (!check(TOKEN_RIGHT_BRACE) && !check(TOKEN_EOF)) {
+    declaration();
+  }
+  consume(TOKEN_RIGHT_BRACE, "Expect '}' after block.");
+}
+
 static void var_declaration() {
   uint8_t global = parse_variable("Expect variable name");
 
@@ -414,6 +425,10 @@ static void declaration() {
 static void statement() {
   if (match(TOKEN_PRINT)) {
     print_statement();
+  } else if (match(TOKEN_LEFT_BRACE)) {
+    begin_scope();
+    block();
+    end_scope();
   } else {
     expression_statement();
   }
