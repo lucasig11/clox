2729264710a28778aa03b7d3f329dced567ca689
lucas <lucasfonsecard@icloud.com>
2021-10-11 14:42:52 -0300

    feat: add closure object

--- src/object.c
+++ src/object.c
@@ -19,6 +19,12 @@ static Obj *allocate_object(size_t size, ObjType type) {
   return object;
 }
 
+ObjClosure *new_closure(ObjFunction *function) {
+  ObjClosure *closure = ALLOCATE_OBJ(ObjClosure, OBJ_CLOSURE);
+  closure->function = function;
+  return closure;
+}
+
 ObjFunction *new_function() {
   ObjFunction *function = ALLOCATE_OBJ(ObjFunction, OBJ_FUNCTION);
   function->arity = 0;
@@ -85,6 +91,10 @@ static void print_function(ObjFunction *function) {
 void print_object(Value value) {
   switch
     OBJ_TYPE(value) {
+    case OBJ_CLOSURE: {
+      print_function(AS_CLOSURE(value)->function);
+      break;
+    }
     case OBJ_FUNCTION:
       print_function(AS_FUNCTION(value));
       break;
