3ba82beff13b4c38783b5175e9f365b37cba3132
lucas <lucasfonsecard@icloud.com>
2021-10-27 16:52:18 -0300

    build: rename watch script

--- watch.sh
+++ watch.sh
@@ -10,7 +10,7 @@ echo "Testing $chap"
 
 if ! watchexec --version &> /dev/null
 then
-    watchexec -Nc -e c,h "make config=release && dart tool/bin/test.dart $chap --interpreter bin/release/clox" 2>/dev/null
+    watchexec -Nc -e c,h "make config=release && dart tool/bin/test.dart $chap --interpreter bin/release/clox 2> /dev/null" 
 else
     echo "[error] watchexec binary not found."
 fi
