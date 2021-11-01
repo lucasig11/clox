995b23fbff9d6a1198ccfe4643b79b006027efb3
lucas <lucasfonsecard@icloud.com>
2021-11-01 07:35:58 -0300

    chore: update watch and test scripts

--- tool/bin/watch.sh
+++ tool/bin/watch.sh
@@ -1,16 +1,8 @@
 #!/usr/bin/sh
 
-chap=$1
-
-if ["$chap" == ""]; then
-    chap="chap24_calls"
-fi
-
-echo "Testing $chap"
-
 if ! watchexec --version &> /dev/null
 then
-    watchexec -Nc -e c,h "make config=release && dart tool/bin/test.dart $chap --interpreter bin/release/clox 2> /dev/null" 
+    watchexec -Nc -e c,h "make config=release && ./tool/bin/run-tests" 
 else
-    echo "[error] watchexec binary not found."
+    echo "[error] watchexec binary not found, install it at https://github.com/watchexec/watchexec"
 fi
